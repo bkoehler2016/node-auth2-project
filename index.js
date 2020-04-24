54402241ee0e3fcdeb72b7d6275087fcf3ffa377
Ben Koehler <bens@Bens-MacBook-Pro.local>
2020-04-23 20:18:12 -0700

    completed MVP

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const server = require('./api/server.js');
 
 //set up ports
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4001;
 
 //listen to the server
 server.listen(PORT, () => console.log(`\n===Running on localhost:${PORT}===\n`))
